befeda0855f6efebef510cc5e011668426b18a9e
wijayakusuma-dev <wijayak07@gmail.com>
2023-08-30 04:46:28 +0700

    update user seeder in DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,12 +13,13 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // \App\Models\User::factory(10)->create();
+        $username = 'wijayak';
 
-        \App\Models\User::factory()->create([
+        \App\Models\User::create([
             'username' => 'wijayak',
             'email' => 'wijayak07@laracuss.com',
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-            'picture' => config('app.avatar_generator_url').'WK',
+            'picture' => config('app.avatar_generator_url').$username,
         ]);
 
         $this->call([
